90f7a4fb4b1d3ebacfd57399c9e42f5e9a1b43fa
prahul25 <palrahulkumar2002@gmail.com>
2024-03-20 08:23:52 +0530

    Project Completion

---
+++ src/app/selectors.js
@@ -0,0 +1,5 @@
+
+
+// Define selector functions to extract data from Redux store
+export const selectAiData = (state) => state.aiData.data;
+export const selectAiDataError = (state) => state.aiData.error;
